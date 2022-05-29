502dab6ee97856493ffc7f81aaac75949b1455fb
DenyAnwar <denyanwardev@gmail.com>
2022-05-30 01:11:55 +0800

    update index.js agar dapat menampung data semua user

--- index.js
+++ index.js
@@ -1,11 +1,66 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+let monicaColors = ['Yellow', 'Pink', 'White', 'Purple'];
+let wendyColors = ['Blue', 'Black', 'Gray'];
+
+let education = new Map();
+education.set("monica",["name: SMA 03, city: Tangerang"]);
+education.set("monica",["name: SMP 02, city: Jakarta, graduate: 2019", ...education.get('monica')]);
+education.set("monica",["name: SD 01, city: Jakarta, graduate: 2016", ...education.get('monica')]);
+
+education.set("wendy",["name: Universitas Maju, city: Tangerang"]);
+education.set("wendy",["name: SMA 01, city: Surabaya, graduate: 2016", ...education.get('wendy')]);
+education.set("wendy",["name: SMP 03, city: Bogor, graduate: 2013", ...education.get('wendy')]);
+education.set("wendy",["name: SD 02, city: Bogor, graduate: 2010", ...education.get('wendy')]);
+
+let monicaFavoriteResto = new Set();
+monicaFavoriteResto.add("Bento");
+monicaFavoriteResto.add("Sushi");
+monicaFavoriteResto.add("Pancake");
+monicaFavoriteResto.add("Tempura");
+monicaFavoriteResto.add("Bento");
+monicaFavoriteResto.add("Eggy");
+monicaFavoriteResto.add("Padang");
+monicaFavoriteResto.add("Tteok");
+monicaFavoriteResto.add("Sushi");
+monicaFavoriteResto.add("Sushi");
+
+let wendyFavoriteResto = new Set();
+wendyFavoriteResto.add("Tempura");
+wendyFavoriteResto.add("Bento");
+wendyFavoriteResto.add("Sushi");
+wendyFavoriteResto.add("Pancake");
+wendyFavoriteResto.add("Padang");
+wendyFavoriteResto.add("Katsu");
+wendyFavoriteResto.add("Geprek");
+wendyFavoriteResto.add("Pancake");
+wendyFavoriteResto.add("Eggy");
+
+const firstUser = {
+  name: "Monica",
+  gender: "Female",
+  age: 17,
+  email: "monica@dingdong.com",
+  favoriteColor: monicaColors,
+  isHavePet: true,  
+  education: education.get('monica'),
+  favoriteRestaurant: monicaFavoriteResto,
+}
+
+const secondUser = {
+  name: "Wendy",
+  gender: "Male",
+  age: 23,
+  email: "wendy@dingdong.com",
+  favoriteColor: wendyColors,
+  isHavePet: false,
+  education: education.get('wendy'),
+  favoriteRestaurant: wendyFavoriteResto,
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+const users = [firstUser, secondUser];
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
